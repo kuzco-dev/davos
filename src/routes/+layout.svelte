513b9da070c9a772c29895072c87911d15958383
Kuzco <kuzco.dev@proton.me>
2025-09-11 18:37:54 +0100

    init project (tauri - svelte - tailwind)

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+  import "./app.css";
+</script>
+
+<div class="relative">
+
+  <main>
+    <slot />
+  </main>
+
+  <nav class="fixed bottom-0 left-0 right-0 h-20 ">
+    hello
+  </nav>
+
+</div>
